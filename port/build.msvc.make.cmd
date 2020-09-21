d7321a8d6ed63d08ba4171eb1741732709a63e5e
Grigore Stefan <g_stefan@yahoo.com>
2020-09-22 01:35:40 +0300

    update

--- port/build.msvc.make.cmd
+++ port/build.msvc.make.cmd
@@ -18,5 +18,5 @@ echo "Error: %ACTION%"
 exit 1
 :StepX
 
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-version.static.compile.info
-call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-version.dynamic.compile.info
+call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-version.static.compile
+call :cmdX xyo-cc --mode=%ACTION% @util/quantum-script-extension-version.dynamic.compile
